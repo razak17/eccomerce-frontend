164c6076392b7840d2dbffb3bcc4fab345730336
Razak <moabdulrazak17@gmail.com>
2022-08-21 22:02:38 +0000

    chore: remove array bracket spacing
    format project

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # Eccomerce Frontend Using React and Styled Components
 
 ### Technlogies
+
 - React
 - Styled Components
 - Material UI
--- src/responsive.ts
+++ src/responsive.ts
@@ -1,10 +1,9 @@
-import { css, SimpleInterpolation } from "styled-components";
+import { css, SimpleInterpolation } from 'styled-components';
 
 export const mobile = (props: SimpleInterpolation) => {
-  return css`
-    @media only screen and (max-width: 380px) {
-      ${props}
-    }
-  `;
+	return css`
+		@media only screen and (max-width: 380px) {
+			${props}
+		}
+	`;
 };
-
